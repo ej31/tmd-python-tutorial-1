bb801e2d286f46cdc927a4c798a64c2102e1e03a
jk <successfulleo7@gmail.com>
2023-08-08 16:44:06 +0900

    연습문제3 수정

--- 연습문제3.py
+++ 연습문제3.py
@@ -1,4 +1,4 @@
 # 연습문제 (3)
 print(f"연습문제 3")
 for i in range(1, 10):
-    print(f"{i} {i * 2} {i * 3} {i * 4} {i * 5} {i * 6} {i * 7} {i * 8} {i * 9}")
+    print(f"{i}\t{i * 2}\t{i * 3}\t{i * 4}\t{i * 5}\t{i * 6}\t{i * 7}\t{i * 8}\t{i * 9}")
