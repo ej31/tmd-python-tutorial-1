4b0109389b209e51f1e1271b381fa63826a77731
고재형 <jaehgo93@naver.com>
2023-08-09 01:26:41 +0900

    삼각형 들 추가

--- main.py
+++ main.py
@@ -29,10 +29,26 @@ for i in range(5, 0, -1):
 
 
 
+# 삼각형 만들기
+for i in range(1,6):
+    s = 5 - i
+    t = 2 * i - 1
+    print(" " * s + "*" * t)
 
 
+# 역삼각형 만들기
+for i in range(5, 0, -1):
+    s = 5 - i
+    t = i * 2 - 1
+    print(" " * s + "*" * t)
 
-
+# 연습
+height = 5
+for star in range(1, height):
+    q = height - star
+    w = star * 2
+    e = height - star
+    print("*" * q + " " * w + "*" * e)
 
 
 
