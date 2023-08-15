a34e0d997eb4b071e09061aa27704179708d6b18
yunhye <yhc444@gmail.com>
2023-08-15 15:02:40 +0900

    no.2

--- def_practice.py
+++ def_practice.py
@@ -1,3 +1,4 @@
+#1
 def even_numbers(numbers):
     _numbers = []
     for number in numbers:
@@ -7,3 +8,11 @@ def even_numbers(numbers):
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 _numbers= even_numbers(numbers)
 print(even_numbers(numbers))
+
+#2
+def sum_elements(elements):
+    total = sum(numbers)
+    return total
+numbers = [10, 20, 30, 40, 50]
+total = sum_elements(numbers)
+print(sum_elements(numbers))
