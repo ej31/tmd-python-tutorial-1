5b2b739f6ba5ffb0d5642e173cbd5813b5101b26
BOOK-92GSM0CJCA\eye50 <sujang46eva@gmail.com>
2023-08-16 05:36:01 +0900

    fd

--- practice001.py
+++ practice001.py
@@ -2,7 +2,7 @@
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 odd_numbers = [num for num in numbers if num % 2 != 0]
 print(odd_numbers)
-print("================================================")
+print()
 # 연습문제 2번 : 1차원 리스트에서 최대/최소 찾기
 numbers = [5, 2, 8, 4, 1, 9, 6]
 max_value = numbers[0]
@@ -17,7 +17,7 @@ for num in numbers:
 
 print("최대값:", max_value)
 print("최솟값:", min_value)
-print("================================================")
+print()
 # 연습문제 3번 : 리스트 내 중복 제거
 fruits = ['apple', 'banana', 'orange', 'banana', 'kiwi', 'apple']
 unique_fruits = []
@@ -25,7 +25,7 @@ for g in fruits:
     if g not in unique_fruits:
         unique_fruits.append(g)
 print(unique_fruits)
-print("================================================")
+print()
 
 # 연습문제 4번 : 리스트에서 짝수, 홀수 분리하기
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
@@ -33,19 +33,19 @@ even_numbers = [num for num in numbers if num % 2 == 0]
 odd_numbers = [num for num in numbers if num % 2 != 0]
 print("짝수:", even_numbers)
 print("홀수:", odd_numbers)
-print("================================================")
+print()
 # 연습문제 5번 : 리스트의 모든 요소 곱하기
 numbers = [1, 2, 3, 4, 5]
 result = 1
 for num in numbers:
     result *= num
 print("곱셈결과:", result)
-print("================================================")
+print()
 # 연습문제 6번 : 두 개의 리스트를 조합해 새로운 리스트 생성
 names = ['Alice', 'Bob', 'Charlie']
 scores = [85, 90, 78]
 zipped = zip(names, scores)
 combined_list = list(zipped)
 print(combined_list)
-print("================================================")
+print()
 
