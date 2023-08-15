ac59c326df61e25de89916eb073a18b0121d0ff8
ROTHIRSCH\kimin <0923katomegumi@gmail.com>
2023-08-16 03:06:28 +0900

    Added assignment file for week3.

---
+++ B3 - Assignment03 - 20230815.py
@@ -0,0 +1,13 @@
+# Assigment 03
+
+def sum_elements(elements):
+    total = 0
+    for i in elements:
+        total += i
+
+    return total
+
+
+numbers = [10, 20, 30, 40, 50]
+
+print(sum_elements(numbers))
