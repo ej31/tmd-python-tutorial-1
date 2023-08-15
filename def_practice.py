3e49fe4e8ac4085e2141c4c3c033eb70739293ba
yunhye <yhc444@gmail.com>
2023-08-15 20:53:32 +0900

    no.5

--- def_practice.py
+++ def_practice.py
@@ -38,4 +38,14 @@ def count_vowels(str):
 str = "Hello, Python!"
 print(count_vowels(str))
 
-
+#5
+def alphabet_frequency(s):
+    d = {}
+    for char in s:
+        if char not in d:
+            d[char]= 1
+        else:
+            d[char]+= 1
+    return d
+string = "Hello, Python!"
+print(alphabet_frequency(string))
