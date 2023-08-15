46d97fb3ee3088bb088c83b5f44fbab230f1a119
yunhye <yhc444@gmail.com>
2023-08-15 20:49:18 +0900

    no.4

--- def_practice.py
+++ def_practice.py
@@ -25,4 +25,17 @@ numbers = [34, 12, 56, 78, 23]
 found = find_max_min(numbers)
 print(find_max_min(numbers))
 
+#4
+def count_vowels(str):
+    vowels = set("aeiouAEIOU")
+    count = 0
+
+    for char in str:
+        if char in vowels:
+            count += 1
+    return count
+
+str = "Hello, Python!"
+print(count_vowels(str))
+
 
