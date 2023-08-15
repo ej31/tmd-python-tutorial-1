7af5c63f73593a23203e9b954e13ca73849583ce
yunhye <yhc444@gmail.com>
2023-08-15 15:07:34 +0900

    no.3

--- def_practice.py
+++ def_practice.py
@@ -16,3 +16,13 @@ def sum_elements(elements):
 numbers = [10, 20, 30, 40, 50]
 total = sum_elements(numbers)
 print(sum_elements(numbers))
+
+#3
+def find_max_min(numbers):
+    found = []
+    return max(numbers), min(numbers)
+numbers = [34, 12, 56, 78, 23]
+found = find_max_min(numbers)
+print(find_max_min(numbers))
+
+
