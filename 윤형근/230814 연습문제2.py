1beeb797a9742473835eef51b0f7b4f24f669da7
DESKTOP-0N4H6AO\sooju <ue1700@gmail.com>
2023-08-16 12:20:57 +0900

    .

---
+++ 윤형근/230814 연습문제2.py
@@ -0,0 +1,12 @@
+
+# 2번
+
+def reverse_string(s):
+    reversed_string = s[::-1]
+    return reversed_string
+
+
+example_string ="파이썬"
+
+print(reverse_string(example_string))
+
---
+++ 윤형근/230814 연습문제3.py
@@ -0,0 +1,13 @@
+# 3번
+
+numbers = [10,20,30,40,50]
+
+def sum_elements(elements):
+    sum_total = 0
+    for i in elements:
+        sum_total += i
+    return sum_total
+
+print(sum_elements(numbers))
+        
+
