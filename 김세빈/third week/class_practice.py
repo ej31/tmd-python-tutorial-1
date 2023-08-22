1fff2d7832ab16e561699f3a3b9f0bb0f07d6bed
bB_Ksb <kimsbin4u@gmail.com>
2023-08-22 11:50:42 +0900

    class를 사용하여 기존의 코드(문자열을 사용하는 함수) 합치기

--- 김세빈/third week/class_practice.py
+++ 김세빈/third week/class_practice.py
@@ -1,48 +1,18 @@
-#  class 연습
-# 기존 코드를 class 형태로 묶어내기!
+'''
+컴포지션 - 한 클래스가 다른 클래스의 인스턴스를 포함
+'''
+class Engine:
+    def start(self):
+        print("Engine Start")
 
-class string_util_func:
-    def __init__(self, s: str):
-        self.__string_util = string_util
-        pass
+class Window:
+    def up(self):
+        print("창문이 올라갑니다")
+    def down(self):
+        print("창문이 내려갑니다")
 
-    def alphabet_frequency(string_util):
-        """
-        문자열을 입력받아 각 알파벳 문자의 빈도를 사전 형태로 반환
-        :return:
-        """
-        frec_dict = {}
-        for alpha in string_util:
-            if alpha.isalpha():
-                if alpha in frec_dict:
-                    frec_dict[alpha] += 1
-                else:
-                    frec_dict[alpha] = 1
-        return frec_dict
-
-    def count_vowels(string_util):
-        """
-        문자열을 입력받아 그 중 영어 모음의 개수를 반환하는 함수
-        :return:
-        """
-        vowel_num = 0
-        vowel_list = ('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U')
-        for i in string_util:
-            if i in vowel_list:
-                vowel_num += 1
-        return vowel_num
-
-    def reverse_string(string_util):
-        """
-        문자열을 입력받아 그 문자열을 거꾸로 뒤집은 결과를 반환하는 함수
-        :return:
-        """
-        return string_util[::-1]
-
-string = "Hello, Python!"
-string_kor = "파이썬"
-
-print(string_util_func.alphabet_frequency(string))
-print(string_util_func.count_vowels(string))
-print(string_util_func.reverse_string(string_kor))
-
+class Car:
+    def __init__(self):
+        self.engine = Engine() # has - a 형태
+        self.window = Window()
+# 상속보다는 컴포지션을 좀더 추천함!
